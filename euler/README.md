74c8d6d6442c0291cbeb0f8561469f0d4a3f6779
FluffyXVI <fluffyxvi@gmail.com>
2019-10-14 02:57:51 +1000

    Euler problems #3 #7 #10 #12

--- euler/README.md
+++ euler/README.md
@@ -6,11 +6,13 @@ A small collection of solutions to the problems found on [Project Euler](https:/
 | ------- | ------ |
 | [#1](https://projecteuler.net/problem=1) | Completed |
 | [#2](https://projecteuler.net/problem=2) | Completed |
-| [#3](https://projecteuler.net/problem=3) | In Progress |
+| [#3](https://projecteuler.net/problem=3) | Completed |
 | [#4](https://projecteuler.net/problem=4) | Completed |
-| [#5](https://projecteuler.net/problem=5) | Not Started |
-| [#6](https://projecteuler.net/problem=6) | Not Started |
-| [#7](https://projecteuler.net/problem=7) | Not Started |
+| [#5](https://projecteuler.net/problem=5) | Completed |
+| [#6](https://projecteuler.net/problem=6) | Completed |
+| [#7](https://projecteuler.net/problem=7) | Completed |
 | [#8](https://projecteuler.net/problem=8) | Not Started |
-| [#9](https://projecteuler.net/problem=9) | Not Started |
-| [#10](https://projecteuler.net/problem=10) | Not Started |
+| [#9](https://projecteuler.net/problem=9) | Completed |
+| [#10](https://projecteuler.net/problem=10) | Completed |
+| [#11](https://projecteuler.net/problem=10) | Not Started |
+| [#12](https://projecteuler.net/problem=10) | Completed |
--- euler/euler.hs
+++ euler/euler.hs
@@ -1,3 +1,15 @@
+-- | Check if a number is prime
+-- Utility function used in a few of the below solutions
+-- Todo: This can probably be made more efficient
+-- Current implementation: check every 2 & odd divisor up until the sqrt
+isPrime :: (Integral a) => a -> Bool
+isPrime 1 = False
+isPrime 2 = True
+isPrime n = do
+    let max = floor (sqrt (fromIntegral n))
+    let divisors = [x | x <-2:[3,5..max], n `mod` x == 0]
+    (length divisors) <= 0
+
 -- | Problem #1
 -- List all natural numbers below N that are multiples of 3 or 5
 problem1 :: (Integral a) => a -> a
@@ -13,12 +25,10 @@ problem2 n = do
 
 -- | Problem #3
 -- Find the largest prime factor of the number N
--- This is not working: still trying to find an efficient way to check if numbers are prime
--- This currently just finds the largest divisor of N
 problem3 :: (Integral a) => a -> a
 problem3 n = do
     let max = floor (sqrt (fromIntegral n))
-    let divisors = [x | x <- [2..max], n `mod` x == 0]
+    let divisors = [x | x <- [2..max], n `mod` x == 0, isPrime x]
     maximum divisors
 
 -- | Problem #4
@@ -40,10 +50,11 @@ problem6 n = do
 -- | Problem #7
 -- Find the Nth prime
 -- I'm too lazy to do a good prime testing thing right now so I'm leaving this blank
-problem7 = 0
+problem7 n = do
+    [x | x <- [2..], isPrime x] !! (n-1)
 
 -- | Problem #8
--- Some grid thing I'm too lazy to type out right now
+-- Some giant grid thing I'm too lazy to deal with right now
 
 -- | Problem #9
 -- Find a Pythagorean Triplet such that a^2 + b^2 = c^2 (where a, b, c are all natural)
@@ -52,3 +63,28 @@ problem7 = 0
 problem9 = do
     let triples = [[a, b, c] | a <- [1..1000], b <- [a..1000], let c = 1000 - a - b, (a^2) + (b^2) == (c^2)]
     product (triples !! 0)
+
+-- | Problem #10
+-- Find the sum of all primes below N
+-- This is inefficient for larger numbers -> improve efficiency of isPrime?
+problem10 n = sum (takeWhile (< n) [x | x <- [2..], isPrime x])
+
+-- | Problem #11
+-- Another large grid that I'm not interested in typing out
+
+
+-- | Find the number of divisors a given number has
+-- By iterating up to sqrt(n), we can find half of each 'pair' of divisors
+-- This greatly cuts down some of the processing work
+-- This function can probably be made more efficient with prime factorisation but it's late
+numDivisors :: Int -> Int
+numDivisors d = do
+    let max = floor (sqrt (fromIntegral d))
+    2 * (length ([x | x <- [1..max], d `mod` x == 0]))
+
+-- | Problem #12
+-- Find the first triangle number with over N divisors
+problem12 :: Int -> Int
+problem12 n = do
+    let triangles = scanl (+) 1 [2..]
+    head [x | x <- triangles, numDivisors x > n]
