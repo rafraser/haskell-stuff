efe2383a7b5bf244295ac9d5d9937ffde68a717b
FluffyXVI <fluffyxvi@gmail.com>
2019-10-14 02:13:25 +1000

    Euler problems #5 #6 #9

--- euler/euler.hs
+++ euler/euler.hs
@@ -24,3 +24,31 @@ problem3 n = do
 -- | Problem #4
 -- Find the largest palindrome made from the product of two 3-digit numbers
 problem4 = maximum [x | p1 <- [100..999], p2 <- [100..999], let x = p1 * p2, show x == reverse (show x)]
+
+-- | Problem #5
+-- Find the largest prime number divisible by all numbers from 1 to N
+problem5 n = foldr (lcm) 1 [1..n]
+
+-- | Problem #6
+-- Find the difference between the sum of the squares and the square of the sum for the first N natural numbers
+problem6 :: (Integral a) => a -> a
+problem6 n = do
+    let sumSquares = sum [x^2 | x <- [1..n]]
+    let squareSum = (sum [x | x <- [1..n]]) ^ 2
+    squareSum - sumSquares
+
+-- | Problem #7
+-- Find the Nth prime
+-- I'm too lazy to do a good prime testing thing right now so I'm leaving this blank
+problem7 = 0
+
+-- | Problem #8
+-- Some grid thing I'm too lazy to type out right now
+
+-- | Problem #9
+-- Find a Pythagorean Triplet such that a^2 + b^2 = c^2 (where a, b, c are all natural)
+-- There is exactly one triplet where a + b + c = 1000
+-- Get the product of this triple
+problem9 = do
+    let triples = [[a, b, c] | a <- [1..1000], b <- [a..1000], let c = 1000 - a - b, (a^2) + (b^2) == (c^2)]
+    product (triples !! 0)
